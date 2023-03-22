cb1a71d9fc6b9c3ecffd872597de79b88e5e4064
Daydream0929 <2352869723@qq.com>
2023-03-22 23:47:36 +0800

    分割好数据集和训练集

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
-data_path = './data/data1.xlsx'  # 存储数据路径
-  
+test_path = './data/data0.xlsx'  # 用来测试的数据
+data_source_path = './data/data.xlsx'  # 存储数据路径
+
 # 数据处理
 def data_process():
-    data = pd.read_excel(data_path)
-    print(data)
+    data = pd.read_excel(data_source_path).to_numpy()
+    X = data[:, [1, 2, 3, 6, 7, 8, 9, 14, 19, 21, 22]]
+    y = data[:, [12]] + data[:, [13]] + data[:, [18]] + data[:, [20]]
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    print(X_train)
+    print(X_test)
+    print(y_train)
+    print(y_test)
 
 # 模型训练
 def model_train():
